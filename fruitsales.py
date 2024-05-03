a3c1235119f352e661c1b1dede823dbfc41721e1
Your Name <you@example.com>
2024-05-03 14:02:58 -0400

    commit exercise

--- fruitsales.py
+++ fruitsales.py
@@ -1,3 +1,18 @@
 import pandas as pd
 fruits = pd.DataFrame(columns=['Apples', 'Bananas'])
-fruits
+print(fruits)
+
+fruits_data = {
+    "Apples" : [35, 41],
+    "Bananas" : [21, 34]
+}
+pd.DataFrame(fruits_data)
+
+fruit_sales = pd.DataFrame({
+    "Apples": [35, 41],
+    "Bananas": [21, 34]},
+    index = ["2017 Sales", "2018 Sales"]
+)
+print(fruit_sales)
+
+fruit_sales.to_csv("fruit.csv", index=True)
